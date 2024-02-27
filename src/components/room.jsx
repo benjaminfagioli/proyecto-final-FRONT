4e82f36bc1362ffa333be9a2e8760f86f3de5373
Bruno Tornese <brunoluchi2003@gmail.com>
2024-02-27 15:47:11 -0300

    Creacion del componente room.jsx y armado dela estructura de routes.js

---
+++ src/components/room.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+const Room = ({ image, text, title }) => {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={image} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{text}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Room;
