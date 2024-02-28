2786c17d00d3d176a4fc4030a8257f0d1b1d77e1
benjaminfagioli <benjitafagg@gmail.com>
2024-02-28 00:58:08 -0300

    roomCards added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Room from "./components/room";
+const URL_BASE = "http://localhost:8080";
 
 const App = () => {
+  const [rooms, setRooms] = useState([]);
+  const getRooms = async (set) => {
+    try {
+      const data = await fetch(`${URL_BASE}/rooms/allrooms`);
+      const results = await data.json();
+      console.log(results);
+      set(results);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  useEffect(() => {
+    getRooms(setRooms);
+  }, []);
   return (
     <>
       <Navbar />
+      <div id="roomCardsContainer">
+        {rooms.map((r) => (
+          <Room image={r.images[0]} title={r.number} text={r.description} />
+        ))}
+      </div>
     </>
   );
 };
--- src/components/room.jsx
+++ src/components/room.jsx
@@ -3,13 +3,14 @@ import Card from "react-bootstrap/Card";
 
 const Room = ({ image, text, title }) => {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={image} />
+    <div style={{ height: "100%" }} className="roomCard">
+      <div className="imgContainer">
+        <Card.Img variant="top" src={image} />
+      </div>
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{text}</Card.Text>
+        <Card.Title>Habitación n°{title}</Card.Title>
       </Card.Body>
-    </Card>
+    </div>
   );
 };
 
