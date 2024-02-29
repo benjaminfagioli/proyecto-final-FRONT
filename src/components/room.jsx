6823b61d2c078581c3303efcdf4109f521553cdf
Bruno Tornese <brunoluchi2003@gmail.com>
2024-02-29 15:43:45 -0300

    Acomodado de room.jsx

--- src/components/room.jsx
+++ src/components/room.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import "../styles/rooms.css";
 
 const Room = ({ image, text, title }) => {
   return (
-    <div style={{ height: "100%" }} className="roomCard">
+    <div className="roomCard">
       <div className="imgContainer">
         <Card.Img variant="top" src={image} />
       </div>
-      <Card.Body>
-        <Card.Title>Habitación n°{title}</Card.Title>
+      <Card.Body className="d-flex flex-column justify-content-between">
+        <div>
+          <Card.Title>Habitación n°{title}</Card.Title>
+          <Card.Text>{text}</Card.Text>
+        </div>
+        <i class="bi bi-box-arrow-in-up-right"></i>
       </Card.Body>
     </div>
   );
