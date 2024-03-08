a88d345077f407719c5b8dce55fd566977088b48
benjaminfagioli <benjitafagg@gmail.com>
2024-03-07 23:10:57 -0300

    loginView con form

--- src/utils/registerUsers.js
+++ src/utils/registerUsers.js
@@ -5,6 +5,7 @@ const URL_BASE = import.meta.env.VITE_URL_BASE;
 const registerUser = async (userData) => {
   try {
     const response = await axios.post(`${URL_BASE}/users/createUser`, userData);
+    console.log(response);
     return response.data;
   } catch (error) {
     throw new Error("Error al registrar usuario");
