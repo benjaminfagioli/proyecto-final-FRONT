6d395457af0750c32019c1d612742018d7824eba
benjaminfagioli <benjitafagg@gmail.com>
2024-03-28 21:46:46 -0300

    estilos en textareas y bug en eliminar las reservas de un usuario

--- src/utils/deleteAllReserves.js
+++ src/utils/deleteAllReserves.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { URL_BASE } from "../config/config";
 
-const deleteAllReserves = async (id, token) => {
+const deleteAllReserves = async (id) => {
+  const token = localStorage.getItem("token");
   try {
     await axios.patch(`${URL_BASE}/rooms/deleteManyReserves/${id}`, null, {
       headers: {
