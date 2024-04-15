65e57c3ce0fb8cb13e36331e991b5a56cc3b624c
benjaminfagioli <benjitafagg@gmail.com>
2024-04-15 20:28:32 -0300

    imageComponent actualizado y modales para admin con imagenes en tiempo real

--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 
-import { useState } from "react";
+// import { useState } from "react";
 
 const ImageComponent = ({ notFoundSrc, src, ...imageAttributes }) => {
-  const [imgSrc, setImgSrc] = useState(src);
+  // const [imgSrc, setImgSrc] = useState(src);
   return (
-    <img
-      {...imageAttributes}
-      src={imgSrc || notFoundSrc}
-      onError={() => {
-        setImgSrc(notFoundSrc);
-      }}
-    />
+    <figure className="imageComponent">
+      <img
+        {...imageAttributes}
+        src={src}
+        onError={(e) => {
+          e.target.classList.add("d-none");
+          e.target.classList.remove("d-block");
+          e.target.closest("figure").style.backgroundSize = "cover";
+          e.target.closest("figure").style.aspectRatio = "1/1";
+          console.log((e.target.closest("figure").style.aspectRatio = "1 / 1"));
+        }}
+        onLoad={(e) => {
+          e.target.classList.remove("d-none");
+          e.target.classList.add("d-block");
+          e.target.closest("figure").style.backgroundSize = 0;
+          // console.log((e.target.closest("figure").style.aspectRatio = ""));
+          console.log((e.target.closest("figure").style.aspectRatio = ""));
+        }}
+      />
+    </figure>
   );
 };
 export default ImageComponent;
